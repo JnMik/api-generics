2cb14a4deb7bee518a7b4802eb6ecd96938a1077
Jean-Michael Cyr <cyrjeanmichael@gmail.com>
2016-11-06 22:23:38 -0500

    - add more logging for debugging in dev environment - better error handling when JSON is invalid on POST or UPDATE.

---
+++ src/Generic/Exception/InvalidJsonException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jm
+ * Date: 06/11/16
+ * Time: 9:30 PM
+ */
+
+namespace Support3w\Api\Generic\Exception;
+
+
+class InvalidJsonException extends \InvalidArgumentException {
+
+} 
